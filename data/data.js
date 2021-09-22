9f2a54788c07e83413af4a4da548fc8703cf5d9f
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2021-09-22 11:53:17 +0200

    initial commit

--- data/data.js
+++ data/data.js
@@ -1,2 +1,11 @@
 "use strict";
+const axios = require('axios');
+const _ = require('lodash');
+const apiKey = '0KywVsIRkpTgbCe3f403oH4EnyF5GqGC3F24kBFo';
+function callApi(search, count, limit, apiKey) {
+    let url = `https://api.fda.gov/drug/event.json?api_key=${apiKey}&`;
+    axios({
+        method: 'get',
+    });
+}
 //# sourceMappingURL=data.js.map
