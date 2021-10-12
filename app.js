8ec555ec4aa79ff15d6dff9a4bd2b763da0da30e
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2021-10-12 12:11:12 +0200

    https

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const path = require('path');
+const https = require('https');
 const router = require('./routes.js');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
-let port = process.env.PORT || 3000;
+const credentials = {
+  key: fs.readFileSync(path.join(__dirname, 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'server.cert')),
+};
+const port = process.env.PORT || 8080;
 
-app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -14,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public', 'css', '/')));
 app.use(express.static(path.join(__dirname, 'public', 'images', '/')));
 app.use(express.static(path.join(__dirname, 'public', 'js', '/')));
 
-app.listen(port, () => {
-  console.log('server listening on port ' + port);
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port, () => {
+  console.log('\nServer listening on port ' + port);
 });
